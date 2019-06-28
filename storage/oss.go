e075cc92df4cd2e5ca9d2858dc9b8c73779ebfac
wzs <zhensheng.five@gmail.com>
2019-06-28 11:33:28 +0800

    添加oss 测试

--- storage/oss.go
+++ storage/oss.go
@@ -16,12 +16,14 @@ type OssClient struct {
 
 type OssClientService interface {
 	// GetObject is 以流的方式读取文件
-	GetObject(name string) (err error)
+	GetObject(bucketID, fileID string) (buf []byte, err error)
 	// PutObject is 上传文件
-	PutObject(name, content string) (err error)
+	PutObject(bucketID, fileID string, buf []byte) (err error)
 }
 
-func NewOssClient(endpoint, accessKeyID, accessKeySecret string) (client *OssClient, err error) {
+var _ OssClientService = &OssClient{}
+
+func NewOssClient(endpoint, accessKeyID, accessKeySecret string) (client OssClientService, err error) {
 	ossClient, err := oss.New(endpoint, accessKeyID, accessKeySecret)
 	if err != nil {
 		return client, err
@@ -34,18 +36,18 @@ func NewOssClient(endpoint, accessKeyID, accessKeySecret string) (client *OssCli
 	}, nil
 }
 
-func (o *OssClient) getBucket(bucketName string) (bucket *oss.Bucket, err error) {
-	bucket, err = o.Bucket(bucketName)
+func (o *OssClient) getBucket(bucketID string) (bucket *oss.Bucket, err error) {
+	bucket, err = o.Bucket(bucketID)
 	return bucket, err
 }
 
-func (o *OssClient) GetObject(bucketName, fileName string) (buf []byte, err error) {
+func (o *OssClient) GetObject(bucketID, fileID string) (buf []byte, err error) {
 	buf = make([]byte, 0)
-	bucket, err := o.getBucket(bucketName)
+	bucket, err := o.getBucket(bucketID)
 	if err != nil {
 		return buf, err
 	}
-	body, err := bucket.GetObject(fileName)
+	body, err := bucket.GetObject(fileID)
 	if err != nil {
 		return buf, err
 	}
@@ -56,12 +58,12 @@ func (o *OssClient) GetObject(bucketName, fileName string) (buf []byte, err erro
 	return buf, err
 }
 
-func (o *OssClient) PutObject(bucketName, fileName string, buf []byte) (err error) {
-	bucket, err := o.getBucket(bucketName)
+func (o *OssClient) PutObject(bucketID, fileID string, buf []byte) (err error) {
+	bucket, err := o.getBucket(bucketID)
 	if err != nil {
 		return err
 	}
-	err = bucket.PutObject(fileName, bytes.NewReader(buf))
+	err = bucket.PutObject(fileID, bytes.NewReader(buf))
 	if err != nil {
 		return err
 	}
--- storage/oss_test.go
+++ storage/oss_test.go
@@ -12,11 +12,29 @@ func Test_OssClienta_GetObject(t *testing.T) {
 	var endpoint = "oss-cn-beijing.aliyuncs.com"
 	var accessKeyID = os.Getenv("accrss_key_id")
 	var accessKeySecret = os.Getenv("accrss_key_secret")
+	if accessKeyID == "" || accessKeySecret == "" {
+		return
+	}
 	Convey("Normal", t, func() {
 		client, err := NewOssClient(endpoint, accessKeyID, accessKeySecret)
 		So(err, ShouldBeNil)
 		buf, err := client.GetObject("xgxw", "todo.md")
 		So(err, ShouldBeNil)
 		fmt.Println(string(buf))
+	})
+}
+
+func Test_OssClienta_PutObject(t *testing.T) {
+	var endpoint = "oss-cn-beijing.aliyuncs.com"
+	var accessKeyID = os.Getenv("accrss_key_id")
+	var accessKeySecret = os.Getenv("accrss_key_secret")
+	if accessKeyID == "" || accessKeySecret == "" {
+		return
+	}
+	Convey("Normal", t, func() {
+		client, err := NewOssClient(endpoint, accessKeyID, accessKeySecret)
+		So(err, ShouldBeNil)
+		err = client.PutObject("xgxw", "todo.md", []byte("this is test content"))
+		So(err, ShouldBeNil)
 	})
 }
