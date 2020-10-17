500e272e2b0da1a3c3b2a391570987a0d40bbc69
wzs <zhensheng.five@gmail.com>
2020-10-17 13:21:35 +0800

    error 添加IError, 组织内使用此error类型以包含Code

--- errors/errors.go
+++ errors/errors.go
@@ -12,28 +12,43 @@ var (
 )
 
 type (
+	// IError is 自定义错误接口
+	IError interface {
+		Error() string
+		Code() int
+	}
 	// Error is 自定义错误信息
 	Error struct {
-		Code       int   `json:"code"`
-		InnerError error `json:"-"`
+		code       int
+		innerError error
 	}
 )
 
+var _ error = new(Error)
+var _ IError = new(Error)
+
 // Error is 转换为字符串
 func (e Error) Error() string {
-	return fmt.Sprintf("code: %d, err: %s", e.Code, e.InnerError.Error())
+	return fmt.Sprintf("code: %d, err: %s", e.code, e.innerError.Error())
 }
 
-func New(code int, err error) Error {
+// Code is 转换为字符串
+func (e Error) Code() int {
+	return e.code
+}
+
+// New 创建一个内部error
+func New(code int, err error) IError {
 	return Error{
-		Code:       code,
-		InnerError: err,
+		code:       code,
+		innerError: err,
 	}
 }
 
+// Code 如果error是IError类型, 则获取error
 func Code(err error) int {
 	if e, ok := err.(Error); ok {
-		return e.Code
+		return e.code
 	}
 	return Unkown
 }
